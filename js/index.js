774b0c76a0d396484427672334d0b1cc3f37d2bb
iqbal-hossain <mdiqbalhossain5170@gmail.com>
2022-03-01 02:50:18 +0600

    some comments added

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
+
+//Those are some functions for validation 
 const emthyError = error => {
     document.getElementById('emthy-error').innerText = error;
-
 }
 
 const error = errorMsg => {
@@ -10,10 +11,12 @@ const spinner = spinnerstyle => {
     document.getElementById('spinner').style.display = spinnerstyle;
 }
 
+
 const container = document.getElementById('container')
 const containerDetails = document.getElementById('container-details');
 
 
+//Data loading function.
 const loadPhone = async () => {
     spinner('block')
     const input = document.getElementById('input');
@@ -35,6 +38,7 @@ const loadPhone = async () => {
     }
 }
 
+
 // Data displaying function
 const displayData = phones => {
     if(!phones.length){
@@ -44,20 +48,21 @@ const displayData = phones => {
       else{
          emthyError('');
       }
+
     container.innerHTML = '';
     error('')
     phones?.forEach(phone => {
 
        
         
-        console.log(phone);
+        // console.log(phone);
 
         const div = document.createElement('div');
         div.classList.add('col-lg-4');
         div.classList.add('col-md-6');
         div.classList.add('my-4');
                 
-        // div.classList.add('my-2')
+      
         div.innerHTML = `
         <div class="card border-0 " style="width: 18rem;">
             <img src="${phone.image}" class="card-img-top" alt="...">
@@ -66,7 +71,6 @@ const displayData = phones => {
             <p class="card-text fw-bold text-success bg-opacity-25">${phone.brand}</p>
             <button class="btn-success p-2 border-0 rounded" onclick="getDetails('${phone.slug}')">See Details</button>
             </div>
-            
         </div>
         `;
         
@@ -78,24 +82,26 @@ const displayData = phones => {
     
 }
 
-
+//Details data loading functions
 const getDetails = id => {
   
   const url = `https://openapi.programming-hero.com/api/phone/${id}`
-
   fetch(url)
   .then(res => res.json())
   .then(data => displayDitails(data.data))
 }
 
+
+//Displaying details data.
 const displayDitails = feature => {
-    console.log(feature);
+    // console.log(feature);
     containerDetails.innerHTML = '';
 
     const div = document.createElement('div');
     container.innerHTML = '';
 
   
+   //funtion for get sensorrs array elements.
    function sensorsItem(){
         const sensors=  feature.mainFeatures.sensors.map(item => item)
         return sensors;
@@ -113,12 +119,15 @@ const displayDitails = feature => {
       <div class="card-body">
         <h5 class="card-title">${feature.name}</h5>
         <p class="card-text"><span>Display size: ${feature.mainFeatures.displaySize}</span></p>
+        
         <p class="card-text"><span>chipset: ${feature.mainFeatures.chipSet? feature.mainFeatures.chipSet: "It's doesn't have chipset"}</span></p>
 
         <p class="card-text"><span>Memory: ${feature.mainFeatures.memory? feature.mainFeatures.memory: "It's doesn't have memory"}</span></p>
 
         <p class="card-text"><small class="text-muted">Realese: ${feature.releaseDate? feature.releaseDate:"No release date found"}</small></p>
+
         <p>Sensors:${sensorsItem()}</p>
+
         <ul>
         <ol>Bluetooth: ${feature.others.Bluetooth}</ol>
         <ol>GPS: ${feature.others.GPS}</ol>
@@ -127,6 +136,7 @@ const displayDitails = feature => {
         <ol>USB: ${feature.others.USB}</ol>
         <ol>WLAN: ${feature.others.WLAN}</ol>
         </ul>
+
       </div>
     </div>
   </div>
